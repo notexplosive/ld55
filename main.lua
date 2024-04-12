6dcda9d9e55257450177ef9b8863dfb66baa6253
notexplosive <thenotexplosive@gmail.com>
2024-04-12 10:14:37 -0700

    Initial commit: new SokoMaker Project

---
+++ main.lua
@@ -0,0 +1,58 @@
+local exports = {}
+
+function exports.onMove(move)
+
+end
+
+
+function exports.onEntityDestroyed(entity)
+
+end
+
+
+function exports.onRoomStateChanged(key)
+
+end
+
+
+function exports.onInput(input)
+
+end
+
+
+function exports.onLoadLevel()
+
+end
+
+
+function exports.onLoadCheckpoint()
+
+end
+
+
+function exports.onEnter()
+
+end
+
+
+function exports.onTurn()
+
+end
+
+
+function exports.onLeave()
+
+end
+
+
+function exports.onStart()
+
+end
+
+
+function exports.onUpdate(dt)
+
+end
+
+return exports
+
