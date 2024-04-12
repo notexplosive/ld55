6ba377ee59dd4df821238d39b97b1c52846dfdf2
notexplosive <thenotexplosive@gmail.com>
2024-04-12 11:56:34 -0700

    fix run script

--- ursa_run.bat
+++ ursa_run.bat
@@ -1,2 +1,2 @@
 :: Assumes SokoMaker exists next to this directory
-..\sokomaker\SokoMakerEditor\bin\Release\net6.0 --modDirectory="." --configuration Release
+..\sokomaker\SokoMakerEditor\bin\Release\net6.0\SokoMakerEditor --modDirectory="." --configuration Release
