9f76426e8022e15b6a7f2a36dfcc2988835dcb7f
notexplosive <thenotexplosive@gmail.com>
2024-04-13 13:43:21 -0700

    update updater again

--- update.bat
+++ update.bat
@@ -22,10 +22,10 @@ git pull
 cd /d ".."
 echo This should be your SokoMaker root directory: %cd%
 
-dotnet run --project LuaDoc --repoPath=%cd%
-xcopy "Manual\snippets.json" "%src%\.vscode\luasnippets.code-snippets" /Y
+:: dotnet run --project LuaDoc --repoPath=%cd%
+:: xcopy "Manual\snippets.json" "%src%\.vscode\luasnippets.code-snippets" /Y
 
-dotnet run --project LuaDoc --repoPath=%cd%
+:: dotnet run --project LuaDoc --repoPath=%cd%
 
 dotnet build SokoMakerEditor --configuration Release
 endlocal
