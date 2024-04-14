08985c00ee984e94b870e18c9fd58ea699b1f56c
notexplosive <thenotexplosive@gmail.com>
2024-04-14 16:40:09 -0700

    change ladders/trapdoors

--- behaviors/teleport_room.lua
+++ behaviors/teleport_room.lua
@@ -1,12 +1,21 @@
 local player = require "library.player"
 local exports = {}
 
-function exports.onActivate(self, args)
-    
+local function warp(x, y)
     local entity = player.instance()
-    entity.gridPosition = Soko:gridPosition(self.state["position_x"], self.state["position_y"])
+    entity.gridPosition = Soko:gridPosition(x, y)
     local room = World:getRoomAtGridPosition(entity.gridPosition)
     player.moveToRoom(room)
+    World.camera:snapToRoom(room)
+    player.move(Soko.DIRECTION.DOWN)
+end
+
+function exports.onActivate(self, args)
+    warp(self.state["position_x"], self.state["position_y"])
+end
+
+function exports.onFailDrop(self, args)
+    warp(self.state["position_x"], self.state["position_y"])
 end
 
 return exports
